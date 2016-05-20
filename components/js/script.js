241416729c2174e1dbb3de1cce604b6f4d184a7a
andras varadi <andris@varadi.tv>
2016-05-20 08:22:11 +0200

    setting up fonts and additional moduls header is ready contet is getting ready

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,62 @@
 // JavaScript Document
-function test() {
-    return "hi ";
+
+  var topoffset = 90;
+// Animated Scrolling
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset
+        }, 1000);
+        return false;
+      } // target.length
+    } //location hostname
+  }); //on click
+
+  //highlight navigation
+  $(window).scroll(function() {
+    var windowpos = $(window).scrollTop() + topoffset;
+    $('nav li a').removeClass('active');
 	
-}
+	    if (windowpos > $('#link1').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link1"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#link2').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link2"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#link3').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link3"]').addClass('active');
+    } //windowpos
+
+    if (windowpos > $('#link4').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link4"]').addClass('active');
+    } //windowpos
+	
+    if (windowpos > $('#link6').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link6"]').addClass('active');
+    } //windowpos
+    if (windowpos > $('#link7').offset().top) {
+      $('nav li a').removeClass('active');
+      $('a[href$="#link7"]').addClass('active');
+    } //windowpos
+
+
+  }); //window scroll
+  
+  
+  $('#main-carousel').owlCarousel({
+    items:1,
+    lazyLoad:false,
+    loop:true,
+	autoplay: false,
+	nav:false,
+});
