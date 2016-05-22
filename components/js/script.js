9964244e7da538fdba3887517aa317730c85c583
Varadi Andris <andris@varadi.tv>
2016-05-22 13:06:40 +0200

    all sections are getting ready

--- components/js/script.js
+++ components/js/script.js
@@ -60,3 +60,24 @@
 	autoplay: false,
 	nav:false,
 });
+
+  $(".owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+	  autoPlay : true,
+	  stopOnHover : true,
+	  lazyLoad : true,
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+navigationText: [
+      "<i class='fa fa-angle-left'></i>",
+     "<i class='fa fa-angle-right'></i>"
+      ],
+	    });
